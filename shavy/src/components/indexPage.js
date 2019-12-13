81dc4f59c866e28abc3b49e2cd72058190b94e12
shatakshiLonkar <shatakshi.lonkar@synerzip.com>
2019-12-13 12:47:13 +0530

    Index component changes:

--- shavy/src/components/indexPage.js
+++ shavy/src/components/indexPage.js
@@ -1,69 +1,62 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
 import lummi from "../assets/images/Lummi.jpg";
 import design from "../assets/images/Design.jpg";
 import decore from "../assets/images/Decore.PNG";
 import staging from "../assets/images/Staging.JPG";
 import { Typography } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
 import "../styles/indexPage.css";
+
 const useStyles = makeStyles(theme => ({}));
 
 const IndexPageComponent = () => {
   const classes = useStyles();
 
   return (
-    <>
-      <div>
-        <Paper className="paperClass">
-          <div>
-            <Grid>
-              <Button color="inherit">Home</Button>
-              <Button color="inherit">About</Button>
-              <Button color="inherit">Shop</Button>
-              <Button color="inherit">Contact</Button>
-              <br></br>
-              <div>
-                <Typography className="typoClass">Shavy</Typography>
-                <Typography className="typoClass">Give that decore</Typography>
-              </div>
-            </Grid>
-          </div>
-        </Paper>
-
-        <Grid container md={6}>
-          <Grid item md={4}>
-            <img className="lummieClass" src={lummi}></img>
-          </Grid>
-          <Grid item md={2}>
-            <div>
-              <Typography variant="h4" style={{ marginLeft: "500px" }}>
-                Welcome to Shavy
-              </Typography>
-            </div>
-          </Grid>
-        </Grid>
-        <Grid className="imageGridClass" container md={12}>
-          <Grid item md={4}>
-            <img className="imgClass" src={design}></img>
-            <Typography>Design</Typography>
-          </Grid>
-          <Grid item md={4}>
-            <img className="imgClass" src={decore}></img>
-            <Typography>Decore</Typography>
-          </Grid>
-          <Grid item md={4}>
-            <img className="imgClass" src={staging}></img>
-            <Typography>Staging</Typography>
-          </Grid>
-        </Grid>
-        <div className="divClass">
-          <img className="decoreClass" src={decore}></img>
-        </div>
+    <div>
+      <div class="divBgColor">
+        <Button>Home</Button>
+        <Button>Abput</Button>
+        <Button>Shop</Button>
+        <Button>Contact</Button>
+        <Typography className="textAlignCenter">shavy</Typography>
+        <Typography className="textAlignCenter">Girl that Decore</Typography>
       </div>
-    </>
+      <Box display="flex" p={1} bgcolor="#eeffcc">
+        <img className="lummieClass" src={lummi}></img>
+        <Typography className="welcomeTypoClass">Welcome to shavy</Typography>
+      </Box>
+      <Box display="flex" p={1} bgcolor="background.paper">
+        <Box p={1}>
+          <img className="designClass" src={design}></img>
+          <Typography className="designTypoClass">Design</Typography>
+        </Box>
+        <Box p={1}>
+          <img className="imgClass" src={decore}></img>
+          <Typography className="decoreTypoClass">Decore</Typography>
+        </Box>
+        <Box p={1}>
+          <img className="stagingClass" src={staging}></img>
+          <Typography className="stagingTypoclass">Staging</Typography>
+        </Box>
+      </Box>
+      <div className="instaDivClass">
+        <Typography>Instagram</Typography>
+        <Box display="flex" bgcolor="#e6faff">
+          <Box p={1}>
+            <img className="img1Class" src={design}></img>
+          </Box>
+          <Box p={1}>
+            <img className="imgClass" src={decore}></img>
+          </Box>
+          <Box p={1}>
+            <img className="imgClass" src={staging}></img>
+          </Box>
+        </Box>
+      </div>
+    </div>
   );
 };
 export default IndexPageComponent;
